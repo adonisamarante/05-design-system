6ab8bb8838cb339b159921c0dde6dc01335683f2
adonisamarante <adonisamarante@gmail.com>
2025-06-09 16:58:41 -0300

    feat: text field for src instead of obj

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -4,10 +4,16 @@ import { Avatar, AvatarProps } from '@ignite-ui/react'
 export default {
   title: 'Data display/Avatar',
   component: Avatar,
-
   args: {
     src: 'https://github.com/adonisamarante.png',
     alt: 'Adonis Amarante',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
   },
 } as Meta<AvatarProps>
 
