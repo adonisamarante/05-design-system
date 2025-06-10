79211b1c7167d1c877ebff02ca776dd393a54f8e
adonisamarante <adonisamarante@gmail.com>
2025-06-10 15:07:35 -0300

    chore: accessibility addon for interface

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -16,7 +16,8 @@ const config: StorybookConfig = {
   ],
   "addons": [
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions')
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   "framework": {
     "name": getAbsolutePath('@storybook/react-vite'),
