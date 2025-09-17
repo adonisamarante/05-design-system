b9bbc19df9041ba50738b90b724fb6f11a0bdd4b
adonisamarante <adonisamarante@gmail.com>
2025-09-17 11:27:39 -0300

    feat: no description option

--- packages/docs/src/stories/Toast.stories.tsx
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -4,9 +4,18 @@ import { Toast, ToastProps } from '@ignite-ui/react'
 export default {
   title: 'Data Display/Toast',
   component: Toast,
-  args: {
-    children: 'Agendamento realizado',
-  },
+  args: {},
 } as Meta<ToastProps>
 
-export const Primary: StoryObj<ToastProps> = {}
+export const Primary: StoryObj<ToastProps> = {
+  args: {
+    title: 'Agendamento realizado',
+    description: 'Quarta-feira, 23 de Outubro às 16h',
+  },
+}
+
+export const NoDescription: StoryObj<ToastProps> = {
+  args: {
+    title: 'Cadastrado com sucesso!',
+  },
+}
